e798bb5d7fc1c8d54f0b658de6080c5af975d954
Vitiok2009 <viktorpts1vol@gmail.com>
2026-04-30 20:32:53 +0300

    feat: add search film

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Autofocus } from '../../directives/autofocus';
+import { FilmsService } from '../../services/film';
 
 @Component({
   selector: 'app-search',
@@ -7,4 +8,10 @@ import { Autofocus } from '../../directives/autofocus';
   templateUrl: './search.html',
   styleUrl: './search.scss',
 })
-export class Search {}
+export class Search {
+  filmService = inject(FilmsService);
+  onSearch(event: any) {
+    const text = (event.target as HTMLInputElement).value;
+    this.filmService.searchItem.set(text);
+  }
+}
